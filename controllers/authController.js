f04da6a6d75c7636d3ae82dcf5c456dc132de2d2
Rahul <rajamohanty12345@gmail.com>
2024-03-19 04:21:16 +0530

    google callback url changed

--- controllers/authController.js
+++ controllers/authController.js
@@ -105,7 +105,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8000/api/v1/oauth2/google/callback",
+      callbackURL: "https://work-tracker-backend.onrender.com/api/v1/oauth2/google/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       const email = profile.emails[0].value;
